416f74e8517aecfaff584bb59a9e04cc863f5d56
willrhyd <34925929+willrhyd@users.noreply.github.com>
2021-09-08 22:23:50 +0100

    google domain added

--- vue-app/src/axios.js
+++ vue-app/src/axios.js
@@ -3,5 +3,5 @@ import axios from 'axios'
 import VueAxios from 'vue-axios';
 
 Vue.use(VueAxios, axios);
-axios.defaults.baseURL = 'https://free-pmc.herokuapp.com';
+axios.defaults.baseURL = 'http://trainingappserver.uk';
 axios.defaults.withCredentials = true;
