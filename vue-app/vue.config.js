64e609280e234c0879cfe17bd0a3b43b3ce27b66
willrhyd <34925929+willrhyd@users.noreply.github.com>
2021-09-09 11:45:06 +0100

    config DNS

--- vue-app/vue.config.js
+++ vue-app/vue.config.js
@@ -1,8 +1,8 @@
 
 // vue.config.js file to be placed in the root of your repository
-// 
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/trainingapp-prod/'
-//     : '/'
-// }
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/trainingapp-prod/'
+    : '/'
+}
